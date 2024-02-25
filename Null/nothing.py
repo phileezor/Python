345974044e31d2889a9af9be97a87fc55f4dc906
Phil <phileezor@gmail.com>
2024-02-25 23:25:09 +0000

    Env. check and a few execises

---
+++ Null/nothing.py
@@ -0,0 +1,23 @@
+print("\n\nNothing to see here, just set up the repo for Python \nand tested the environment for CS50 works \n as well as my personal environment on my PC.\n\n")
+
+# Null Exercise for CS50P - Hello!
+
+# Readers Name
+readersName = input("What's your name? \n")
+
+# Removes whitespace, capitalizes, titles the readers input.
+readersName = readersName.strip().capitalize().title()
+
+# Split user's name into first name and last name
+name, surname = readersName.split(" ")
+
+
+# Repository reader text
+print("\n\nHello,", name, "what brings you here?" 
+      "\nA new \"READER\" in this forgotten Repository? I'm surprised!!\n")
+
+# docs.python.org
+
+
+# arguments - actual function pass
+# parameter - potential function pass
